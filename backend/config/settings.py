c52246d9bad7c9c48de2f656882063ae7ce72c90
bijo <bijoshaji92@gmail.com>
2026-09-23 12:59:41 +0530

    Fix production allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
+    "localhost,127.0.0.1,ai-jobmatch-aval.onrender.com"
 ).split(",")
 
 # Application definition
