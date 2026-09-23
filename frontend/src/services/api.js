668b9419801e0d9fb541e833e81bf91f3b170755
bijo <bijoshaji92@gmail.com>
2026-09-23 13:25:27 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: `${API_BASE_URL.replace(/\/$/, "")}/api`,
   headers: {
     "Content-Type": "application/json",
   },
